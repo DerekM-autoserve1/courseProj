e013fbd584a694d76966e9c447a7ac5ef0deb9b4
Derek Miller <derekm@autoserve1.com>
2022-03-26 13:41:16 -0400

    Added shoppinglist service and changed the connection of adding and displaying new recipes

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/Ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,18 +9,18 @@ import { Ingredient } from '../shared/Ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
 
-   ingredients : Ingredient[] = [
-    new Ingredient('apples', 5),
-    new Ingredient('oranges', 8)
-  ];
+   ingredients! : Ingredient[];
 
-  constructor() { }
+  constructor(private slService : ShoppingListService) { }
 
   ngOnInit(): void {
+    this.ingredients = this.slService.getIngredients();
+    this.slService.ingredientsChanged.subscribe(
+      (ingredients:Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    )
   }
 
-  onIngredientAdded(ingredient : Ingredient){
-    this.ingredients.push(ingredient);
-  }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,22 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/Ingredient.model";
+
 export class ShoppingListService{
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+
+    private ingredients : Ingredient[] = [
+        new Ingredient('apples', 5),
+        new Ingredient('oranges', 8)
+      ];
+
+      getIngredients(){
+          return this.ingredients.slice();
+      }
+
+      addIngredient(ingredient:Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+      }
     
 }
