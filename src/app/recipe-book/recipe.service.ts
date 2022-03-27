1e1ca2d443b436e40988dc3eefecd3e3f73a47be
Derek Miller <derekm@autoserve1.com>
2022-03-27 19:39:41 -0400

    added ingredients so that they are visible with the recipe

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/Ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,8 +7,23 @@ export class RecipeService {
 
 
     private recipes : Recipe[] = [
-        new Recipe('test Recipe', 'this is a test', 'https://static01.nyt.com/images/2022/03/23/dining/18Apperex/merlin_202627638_8d05e37f-bba7-4295-a738-138f7f208131-articleLarge.jpg'),
-        new Recipe('GAGN', 'BIG DOINKS', 'https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg')
+        new Recipe(
+            'Spicy Cauliflower wrap', 
+            'Tasty and veggielicious', 
+            'https://static01.nyt.com/images/2022/03/23/dining/18Apperex/merlin_202627638_8d05e37f-bba7-4295-a738-138f7f208131-articleLarge.jpg',
+            [
+                new Ingredient('cauliflower', 10),
+                new Ingredient('sauce', 8)
+            ]),
+        new Recipe(
+            'Its a pizza duh', 
+            'with mushrroms and nice leaves what could go wrong', 
+            'https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg',
+            [
+                new Ingredient('mushroom', 8),
+                new Ingredient('leaves', 12)
+
+            ])
       ];
 
       getRecipes(){
