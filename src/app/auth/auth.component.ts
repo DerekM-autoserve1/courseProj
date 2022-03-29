a92ca4c2439e005cb1d19fe17906b0cd047ae63c
Derek Miller <derekm@autoserve1.com>
2022-03-29 16:18:30 -0400

    Added login capabilties

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -29,25 +30,29 @@ export class AuthComponent implements OnInit {
 
     const email = form.value.email;
     const password = form.value.password;
+    let authObs: Observable<AuthResponseData>;
 
     this.isLoading=true;
 
     if( this.isLoginMode){
-      //..
+      authObs = this.authService.login(email,password);
     } else{
-    this.authService.signUp(email,password)
-    .subscribe(resData => {
+      authObs = this.authService.signUp(email,password);
+    };
+
+    authObs.subscribe(resData => {
       console.log(resData);
-      this.isLoading=false;
-    }, error => {
-      console.log(error);
-      this.error='an Error occured';
-      this.isLoading=false;
-    }
-    );
+      this.isLoading = false;
+    },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    )
 
     form.reset();
   }
 
 }
-}
+
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken : string,
     email : string,
     refreshToken : string,
     expiresIn : string,
     localId : string,
+    registered?:boolean
 }
+
 
 @Injectable({providedIn:'root'})
 export class AuthService{
@@ -22,5 +24,15 @@ export class AuthService{
             returnSecureToken: true
         }
         );
+    }
+
+    login(email:string, password:string){
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDa-Lej2Q9Kj-taykuKMahCwxwCr1v9_Pg',
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        )
     }
 }
