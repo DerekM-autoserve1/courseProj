292b2ac3426c78a812ac4543174ef1e05b162643
Derek Miller <derekm@autoserve1.com>
2022-03-29 13:40:33 -0400

    Added resolver, and fixed some bugs with it (need more review on this)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
+import { RecipeResolverService } from './recipe-book/recipe-resolver.service';
 import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -11,10 +12,10 @@ const routes: Routes = [
   { path: 'recipes', component: RecipeBookComponent, children: [
     {path: '', component: RecipeStartComponent},
     {path: 'new', component: RecipeEditComponent },
-    {path: ':id' , component:RecipeDetailComponent},
+    {path: ':id' , component:RecipeDetailComponent, resolve:[RecipeResolverService]},
     {path: ':id/edit', component: RecipeEditComponent },
   ] },
-  { path: 'shopping-list', component: ShoppingListComponent }
+  { path: 'shopping-list', component: ShoppingListComponent, resolve:[RecipeResolverService] }
 ];
 
 @NgModule({
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipe-book/recipe.service";
 import { Recipe } from "../recipe-book/recipe.model";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -19,15 +19,15 @@ export class DataStorageService {
     }
 
     fetchRecipes(){
-        this.http.get<Recipe[]>('https://ng-course-recipe-book-c5ec0-default-rtdb.firebaseio.com/recipes.json')
+         return this.http.get<Recipe[]>('https://ng-course-recipe-book-c5ec0-default-rtdb.firebaseio.com/recipes.json')
         //rxjs method map in pipe and one in return is a JS map
         .pipe(map(recipes => {
             return recipes.map( recipe => {
                 return{...recipe, ingredients: recipe.ingredients ? recipe.ingredients:[] }
             });
-        }))
-        .subscribe(recipes =>{
-            this.recipeService.setRecipes(recipes);
-        })
+        }),
+        tap(recipes =>{
+            this.recipeService.setRecipes(recipes) })
+        )
     }
 }
