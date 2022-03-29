754c70cc9375e11bb9ad8ba72d092755c7d6700e
Derek Miller <derekm@autoserve1.com>
2022-03-29 16:40:45 -0400

    added authentication for signing up and logging in

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { User } from "./user.model";
+import { Subject } from "rxjs";
+import { tap } from "rxjs/operators";
 
 export interface AuthResponseData {
     idToken : string,
@@ -13,6 +16,8 @@ export interface AuthResponseData {
 
 @Injectable({providedIn:'root'})
 export class AuthService{
+    user = new Subject<User>();
+
     constructor(private http : HttpClient){}
 
     signUp(email:string, password:string){
@@ -23,7 +28,18 @@ export class AuthService{
             password: password,
             returnSecureToken: true
         }
-        );
+        )
+        .pipe(tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+        }
+
+        ))
+    }
+
+    private handleAuthentication(email:string, userId:string, token:string, expiresIn:number){
+        const expDate = new Date(new Date().getTime() + expiresIn * 1000);
+        const user = new User(email, userId, token, expDate);
+        this.user.next(user);
     }
 
     login(email:string, password:string){
@@ -34,5 +50,8 @@ export class AuthService{
             returnSecureToken: true
         }
         )
+        .pipe(tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+        }))
     }
 }
